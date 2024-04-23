dee2969541beed5afc631779e347c27ebb694f3b
Cade Li <cadecode@foxmail.com>
2024-04-23 21:23:32 +0800

    feat(admin): 添加 admin 启动类、测试类

---
+++ ubp-admin/src/main/java/com/github/cadecode/ubp/admin/UniBootAdminApplication.java
@@ -0,0 +1,18 @@
+package com.github.cadecode.ubp.admin;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+/**
+ * 后台服务启动类
+ *
+ * @author Cade Li
+ * @since 2024/4/22
+ */
+@SpringBootApplication
+public class UniBootAdminApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(UniBootAdminApplication.class, args);
+    }
+}
---
+++ ubp-admin/src/test/java/com/github/cadecode/ubp/admin/UniBootAdminApplicationTests.java
@@ -0,0 +1,18 @@
+package com.github.cadecode.ubp.admin;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+/**
+ * 后台服务测试类
+ *
+ * @author Cade Li
+ * @since 2024/4/22
+ */
+@SpringBootTest
+public class UniBootAdminApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+}
