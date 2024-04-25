6e82742b4ba7fa88529a7f7de7664609c9d0f503
Cade Li <cadecode@foxmail.com>
2024-04-25 11:19:07 +0800

    feat(admin): 添加 @MapperScan 注解

--- ubp-admin/src/main/java/com/github/cadecode/ubp/admin/UniBootAdminApplication.java
+++ ubp-admin/src/main/java/com/github/cadecode/ubp/admin/UniBootAdminApplication.java
@@ -1,5 +1,6 @@
 package com.github.cadecode.ubp.admin;
 
+import org.mybatis.spring.annotation.MapperScan;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
@@ -9,6 +10,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
  * @author Cade Li
  * @since 2024/4/22
  */
+@MapperScan({"com.github.cadecode.**.mapper"})
 @SpringBootApplication
 public class UniBootAdminApplication {
 
