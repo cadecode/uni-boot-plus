e8ca356859e853bb6f958a065e5a2db9a338207c
Cade Li <cadecode@foxmail.com>
2024-04-26 12:35:43 +0800

    feat(codegen): 添加启动类、配置文件

---
+++ ubp-codegen/src/main/java/com/github/cadecode/ubp/codegen/UniBootCodegenApplication.java
@@ -0,0 +1,18 @@
+package com.github.cadecode.ubp.codegen;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+/**
+ * 代码生成器启动类
+ *
+ * @author Cade Li
+ * @since 2024/4/25
+ */
+@SpringBootApplication
+public class UniBootCodegenApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(UniBootCodegenApplication.class, args);
+    }
+}
