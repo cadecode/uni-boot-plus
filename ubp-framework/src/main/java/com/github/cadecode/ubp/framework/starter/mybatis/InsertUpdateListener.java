95b2d5d027777ac1818811912d9e80976cf25b00
Cade Li <cadecode@foxmail.com>
2024-05-06 17:32:14 +0800

    feat(framework): MyBatisFlex listener 填充 updateUser

--- ubp-framework/src/main/java/com/github/cadecode/ubp/framework/starter/mybatis/InsertUpdateListener.java
+++ ubp-framework/src/main/java/com/github/cadecode/ubp/framework/starter/mybatis/InsertUpdateListener.java
@@ -1,7 +1,12 @@
 package com.github.cadecode.ubp.framework.starter.mybatis;
 
+import cn.dev33.satoken.stp.StpUtil;
 import com.github.cadecode.ubp.starter.mybatis.listener.BaseInsertUpdateListener;
+import com.github.cadecode.ubp.starter.mybatis.model.BaseFieldOperable;
 import org.springframework.stereotype.Component;
+
+import java.time.LocalDateTime;
+import java.util.Objects;
 
 /**
  * 监听器
@@ -14,13 +19,28 @@ public class InsertUpdateListener extends BaseInsertUpdateListener {
 
     @Override
     public void onInsert(Object entity) {
-        super.onInsert(entity);
-        // TODO updateUser 字段填充
+        if (entity instanceof BaseFieldOperable baseFieldOperable) {
+            baseFieldOperable.setCreateTime(LocalDateTime.now());
+            setLoginId(baseFieldOperable);
+        }
     }
 
     @Override
     public void onUpdate(Object entity) {
-        super.onUpdate(entity);
-        // TODO updateUser 字段填充
+        if (entity instanceof BaseFieldOperable baseFieldOperable) {
+            baseFieldOperable.setUpdateTime(LocalDateTime.now());
+            setLoginId(baseFieldOperable);
+        }
+    }
+
+    /**
+     * 获取登录的 ID
+     */
+    private void setLoginId(BaseFieldOperable baseFieldOperable) {
+        // 从 SaToken 获取
+        Object loginId = StpUtil.getLoginIdDefaultNull();
+        if (Objects.nonNull(loginId)) {
+            baseFieldOperable.setUpdateUser(String.valueOf(loginId));
+        }
     }
 }
