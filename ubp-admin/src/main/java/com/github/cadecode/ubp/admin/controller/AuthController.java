c99844350969d4c58fc8d71395ce07bca16597c7
Cade Li <cadecode@foxmail.com>
2024-05-10 13:56:01 +0800

    feat(admin): 添加 auth 相关接口，登录注销等

---
+++ ubp-admin/src/main/java/com/github/cadecode/ubp/admin/controller/AuthController.java
@@ -0,0 +1,103 @@
+package com.github.cadecode.ubp.admin.controller;
+
+import cn.dev33.satoken.secure.SaSecureUtil;
+import cn.dev33.satoken.stp.SaTokenInfo;
+import cn.dev33.satoken.stp.StpUtil;
+import com.github.cadecode.ubp.admin.bean.po.SysUser;
+import com.github.cadecode.ubp.admin.service.SysUserService;
+import com.github.cadecode.ubp.common.exception.GeneralException;
+import com.github.cadecode.ubp.framework.enums.AuthErrorEnum;
+import com.github.cadecode.ubp.starter.web.annotation.ApiFormat;
+import com.github.cadecode.ubp.starter.web.model.ApiResult;
+import io.swagger.v3.oas.annotations.Operation;
+import io.swagger.v3.oas.annotations.Parameter;
+import io.swagger.v3.oas.annotations.tags.Tag;
+import lombok.RequiredArgsConstructor;
+import org.springframework.validation.annotation.Validated;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.Objects;
+
+/**
+ * 登录校验 控制层
+ *
+ * @author Cade Li
+ * @since 2024/5/10
+ */
+@ApiFormat
+@RequiredArgsConstructor
+@Validated
+@RestController
+@Tag(name = "登录校验接口")
+@RequestMapping("/auth")
+public class AuthController {
+
+    private final SysUserService sysUserService;
+
+    /**
+     * 用户登录
+     *
+     * @return ApiResult
+     */
+    @PostMapping("login")
+    @Operation(description = "用户登录")
+    public SaTokenInfo login(@RequestParam @Parameter(description = "用户 ID") String userId,
+                             @RequestParam @Parameter(description = "密码") String password,
+                             @RequestParam @Parameter(description = "是否记住我") Boolean rememberMe) {
+        // 1. 查询数据库
+        SysUser sysUser = sysUserService.getById(userId);
+        if (Objects.isNull(sysUser)) {
+            throw GeneralException.of(AuthErrorEnum.TOKEN_CREATE_ERROR, "该用户不存在");
+        }
+        // 2. 判断是否启用账户
+        if (Objects.equals(sysUser.getEnableFlag(), false)) {
+            throw GeneralException.of(AuthErrorEnum.TOKEN_CREATE_ERROR, "账号已被关闭");
+        }
+        // 3. 校验密码
+        String securedPassword = SaSecureUtil.sha256(password);
+        if (!Objects.equals(securedPassword, sysUser.getPassword())) {
+            throw GeneralException.of(AuthErrorEnum.TOKEN_CREATE_ERROR, "密码错误");
+        }
+        // 4. 登录
+        StpUtil.login(userId, rememberMe);
+        // 5. 获取 token 相关信息
+        SaTokenInfo tokenInfo = StpUtil.getTokenInfo();
+        // 6. 返回 token 信息
+        return tokenInfo;
+    }
+
+    /**
+     * 判断当前用户是否登录
+     *
+     * @return 是否登录
+     */
+    @GetMapping("is_login")
+    @Operation(description = "判断当前用户是否登录")
+    public Boolean isLogin() {
+        return StpUtil.isLogin();
+    }
+
+    /**
+     * 注销当前用户
+     *
+     * @return ApiResult, 用户主动注销
+     */
+    @GetMapping("logout")
+    @Operation(description = "注销当前用户")
+    public ApiResult<?> logout() {
+        StpUtil.logout();
+        return ApiResult.error(AuthErrorEnum.TOKEN_LOGOUT);
+    }
+
+    /**
+     * 获取当前用户信息
+     *
+     * @return 用户信息
+     */
+    @GetMapping("get_user_info")
+    @Operation(description = "获取当前用户信息")
+    public Object getUserInfo() {
+        // TODO 获取当前用户信息
+        return null;
+    }
+}
