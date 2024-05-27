0feb70ec88d539f9ec028796b85f717da0c99fad
Cade Li <cadecode@foxmail.com>
2024-05-27 13:45:29 +0800

    docs(admin): 用户组表字段添加 parent id、leader、mail 等

--- docs/sql/mysql/admin.sql
+++ docs/sql/mysql/admin.sql
@@ -25,7 +25,13 @@ CREATE TABLE IF NOT EXISTS sys_user
 CREATE TABLE IF NOT EXISTS sys_user_group
 (
     user_group_id   VARCHAR(50)   NOT NULL COMMENT '用户组 ID',
+    parent_group_id VARCHAR(50)   NOT NULL COMMENT '父级 ID',
     user_group_name VARCHAR(50)   NOT NULL COMMENT '用户组名称',
+    order_num       INT           NULL COMMENT '排序',
+    ancestors       VARCHAR(2000) NULL COMMENT '祖先部门',
+    leader          VARCHAR(500)  NULL COMMENT '领导名',
+    mail            VARCHAR(50)   null comment '邮箱',
+    phone           VARCHAR(50)   null comment '电话',
     first_tenant    VARCHAR(50)   NULL COMMENT '第一租户标识（作用于增删改查）',
     other_tenants   VARCHAR(1000) NULL COMMENT '其他租户标识（作用于删改查）',
 
@@ -33,7 +39,8 @@ CREATE TABLE IF NOT EXISTS sys_user_group
     update_time     DATETIME DEFAULT NULL ON UPDATE CURRENT_TIMESTAMP COMMENT '更新时间',
     update_user     VARCHAR(100)  NULL COMMENT '更新人',
     PRIMARY KEY (user_group_id),
-    INDEX idx_user_group_name (user_group_name)
+    INDEX idx_user_group_name (user_group_name),
+    INDEX idx_parent_group_id (parent_group_id)
 ) ENGINE = InnoDB
   DEFAULT CHARSET = utf8mb4
     COMMENT '系统用户组';
