75e4225473551712a96d21a384cbd24d9e085e88
Cade Li <cadecode@foxmail.com>
2024-05-27 13:49:35 +0800

    refactor(admin): 用户组实体添加 parentGroupId 等字段

--- ubp-admin/src/main/java/com/github/cadecode/ubp/admin/bean/po/SysUserGroup.java
+++ ubp-admin/src/main/java/com/github/cadecode/ubp/admin/bean/po/SysUserGroup.java
@@ -18,7 +18,7 @@ import java.time.LocalDateTime;
  * 系统用户组 实体类
  *
  * @author Cade Li
- * @since 2024/5/10
+ * @since 2024/5/24
  */
 @Data
 @Builder
@@ -39,10 +39,46 @@ public class SysUserGroup implements BaseFieldOperable, Serializable {
     private String userGroupId;
 
     /**
-     * 用户名称
+     * 父级 ID
      */
-    @Schema(description = "用户名称")
+    @Schema(description = "父级 ID")
+    private String parentGroupId;
+
+    /**
+     * 用户组名称
+     */
+    @Schema(description = "用户组名称")
     private String userGroupName;
+
+    /**
+     * 排序
+     */
+    @Schema(description = "排序")
+    private Integer orderNum;
+
+    /**
+     * 祖先部门
+     */
+    @Schema(description = "祖先部门")
+    private String ancestors;
+
+    /**
+     * 领导名
+     */
+    @Schema(description = "领导名")
+    private String leader;
+
+    /**
+     * 邮箱
+     */
+    @Schema(description = "邮箱")
+    private String mail;
+
+    /**
+     * 电话
+     */
+    @Schema(description = "电话")
+    private String phone;
 
     /**
      * 第一租户标识（作用于增删改查）
