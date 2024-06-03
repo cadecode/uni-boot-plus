2e69fef9fc4b82f23a11f7bb5b5e7ff7066df66f
Cade Li <cadecode@foxmail.com>
2024-06-03 14:36:51 +0800

    feat(framework): 调整 Jackson LocalDataTime 序列化策略

--- ubp-framework/src/main/java/com/github/cadecode/ubp/framework/config/JacksonConfig.java
+++ ubp-framework/src/main/java/com/github/cadecode/ubp/framework/config/JacksonConfig.java
@@ -1,9 +1,17 @@
 package com.github.cadecode.ubp.framework.config;
 
 import com.fasterxml.jackson.databind.ser.std.ToStringSerializer;
+import com.fasterxml.jackson.datatype.jsr310.deser.LocalDateDeserializer;
+import com.fasterxml.jackson.datatype.jsr310.deser.LocalDateTimeDeserializer;
+import com.fasterxml.jackson.datatype.jsr310.deser.LocalTimeDeserializer;
+import com.fasterxml.jackson.datatype.jsr310.ser.LocalDateSerializer;
+import com.fasterxml.jackson.datatype.jsr310.ser.LocalDateTimeSerializer;
+import com.fasterxml.jackson.datatype.jsr310.ser.LocalTimeSerializer;
 import org.springframework.boot.autoconfigure.jackson.Jackson2ObjectMapperBuilderCustomizer;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+
+import java.time.format.DateTimeFormatter;
 
 /**
  * Jackson配置
@@ -16,11 +24,27 @@ public class JacksonConfig {
 
     @Bean
     public Jackson2ObjectMapperBuilderCustomizer jacksonObjectMapperCustomization() {
-        return jacksonObjectMapperBuilder -> {
+        return builder -> {
             // 关闭空对象序列化失败，如 new Object()
-            jacksonObjectMapperBuilder.failOnEmptyBeans(false);
-            jacksonObjectMapperBuilder.serializerByType(Long.class, ToStringSerializer.instance);
-            jacksonObjectMapperBuilder.serializerByType(Long.TYPE, ToStringSerializer.instance);
+            builder.failOnEmptyBeans(false);
+
+            // Long 序列化为字符串（防止前端溢出）
+            builder.serializerByType(Long.class, ToStringSerializer.instance);
+            builder.serializerByType(Long.TYPE, ToStringSerializer.instance);
+
+            // LocalDate 和 LocalDateTime 的格式化方式
+            DateTimeFormatter dateFormatter = DateTimeFormatter.ofPattern("yyyy-MM-dd");
+            DateTimeFormatter timeFormatter = DateTimeFormatter.ofPattern("HH:mm:ss");
+            DateTimeFormatter dateTimeFormatter = DateTimeFormatter.ofPattern("yyyy-MM-dd HH:mm:ss");
+            // 设置 LocalDate / LocalTime / LocalDateTime 序列化
+            builder.serializers(new LocalDateSerializer(dateFormatter));
+            builder.deserializers(new LocalDateDeserializer(dateFormatter));
+
+            builder.serializers(new LocalTimeSerializer(dateFormatter));
+            builder.deserializers(new LocalTimeDeserializer(dateFormatter));
+
+            builder.serializers(new LocalDateTimeSerializer(dateTimeFormatter));
+            builder.deserializers(new LocalDateTimeDeserializer(dateTimeFormatter));
         };
     }
 }
