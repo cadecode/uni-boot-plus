c0de3fbe6cdd6a22aa73b56497855fa44fbec1e6
Cade Li <cadecode@foxmail.com>
2024-08-04 22:05:21 +0800

    refactor(starter): 关闭 PageHelper 特性：pageSize=0 查全部
    1. 避免数据量太大，关闭此特性
    2. 可在 PageHelper startPage 方法参数中本次开启

--- ubp-starter/ubp-starter-mybatis/src/main/java/com/github/cadecode/ubp/starter/mybatis/config/PageHelperConfig.java
+++ ubp-starter/ubp-starter-mybatis/src/main/java/com/github/cadecode/ubp/starter/mybatis/config/PageHelperConfig.java
@@ -24,8 +24,11 @@ public class PageHelperConfig {
         Properties properties = new Properties();
         // pageNum<=0 查第一页
         properties.setProperty("reasonable", "true");
+
+        /// 避免数据量太大，关闭此特性，可在 startPage 方法参数中本次开启
         // pageSize=0 查全部
-        properties.setProperty("page-size-zero", "true");
+        // properties.setProperty("page-size-zero", "true");
+
         // 自动识别方言
         properties.setProperty("autoRuntimeDialect", "true");
         pageInterceptor.setProperties(properties);
